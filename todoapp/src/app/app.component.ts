c48e7ddbf2cca1e1602b4c15083a3fc6707ae4ea
DrMadWill <nofelsalahov9@gmail.com>
2022-09-13 10:06:01 +0400

    main: To Do App Complated

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -13,7 +13,26 @@ export class AppComponent {
     return this.model.username
   }
 
+
+  isDisblay:boolean = false;
+
   getItems():Array<TodoItem>{
+
+    if(this.isDisblay){
+      return this.getNotCheckItems();
+    }
     return this.model.items
   }
+
+  getNotCheckItems():Array<TodoItem>{
+    return this.model.items.filter(item=>!item.result)
+  }
+
+  addItem(value:string){
+    if(value != ""){
+      let toDo : TodoItem =new TodoItem(value,false)
+      this.model.items.push(toDo);
+    }
+  }
+
 }
--- todoapp/src/app/model.ts
+++ todoapp/src/app/model.ts
@@ -4,15 +4,15 @@ export class Model {
   constructor(){
     this.username = "Will";
     this.items = [
-      new TodoItem("Task Complate", "text-success fa-solid fa-circle-check" ),
-      new TodoItem( "Task Complate", "text-warning fa-solid fa-circle-pause" ),
-      new TodoItem( "Task Complate", "text-danger fa-solid fa-circle-xmark" ),
-      new TodoItem( "Task Complate", "text-danger fa-solid fa-circle-xmark" )
+      new TodoItem("Task Complate", true ),
+      new TodoItem( "Task Complate", false ),
+      new TodoItem( "Task Complate", true ),
+      new TodoItem( "Task Complate", false )
     ]
   }
 }
 
 export class TodoItem{
-   constructor(public description:string,public result:string){
+   constructor(public description:string,public result:boolean){
    }
 }
