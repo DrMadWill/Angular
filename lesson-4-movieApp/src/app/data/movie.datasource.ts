5226d09ebebb1e37527f75a898f3b1c20a13cd3e
DrMadWill <nofelsalahov9@gmail.com>
2022-09-13 14:47:02 +0400

    main:

---
+++ lesson-4-movieApp/src/app/data/movie.datasource.ts
@@ -0,0 +1,13 @@
+import { Movie } from './../models/movie';
+export const Movies:Movie[] = [
+  {id:1,name:"Movie 1"},
+  {id:2,name:"Movie 2"},
+  {id:3,name:"Movie 3"},
+  {id:4,name:"Movie 4"},
+  {id:5,name:"Movie 5"},
+  {id:6,name:"Movie 6"},
+  {id:7,name:"Movie 7"}
+]
+
+
+
--- lesson-4-movieApp/src/app/movies/movies.component.ts
+++ lesson-4-movieApp/src/app/movies/movies.component.ts
@@ -1,11 +1,25 @@
 import { Component } from "@angular/core";
+import { Movies } from "../data/movie.datasource";
+import { Movie } from "../models/movie";
 
 @Component({
-  selector:"movies"  ,
+  selector:"movies",
+  styleUrls:["./movies.component.css"],
   templateUrl:"./movies.component.html"
-
 })
 export class MoviesComponent{
+  title = "Movie List"
+
+  getTitle():string{
+    return this.title
+  }
+  movies : Movie[] = Movies
+
+  selectedMovie!:Movie;
+
+  onSelect(movie:Movie):void{
+    this.selectedMovie = movie;
+  }
 }
 
 
